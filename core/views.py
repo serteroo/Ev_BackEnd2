0d55728bfb1caccb450ecc3f071bb42f04bed591
renato-26 <munozrenato329@gmailcom>
2025-11-07 13:21:46 -0300

    templates2

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from core.scoping import ScopedFilterBackend
-
+from core.models import Dispositivo
 class DispositivoViewSet(ModelViewSet):
     queryset = Dispositivo.objects.all()
     serializer_class = DispositivoSerializer
